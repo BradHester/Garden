031cfdb5b10335679fe07bfe1bd498bc5ab2c93d
Brad_Hester <hesterbj@iinet.net.au>
2018-01-25 22:36:27 +1100

    Initial Commit

--- Index.js
+++ Index.js
@@ -1,26 +1,9 @@
-var sensorLib = require("node-dht-sensor");
+var sensor = require('node-dht-sensor');
 
-var sensor = {
-    sensors: [ {
-        name: "Indoor",
-        type: 11,
-        pin: 17
-    }, {
-        name: "Outdoor",
-        type: 22,
-        pin: 4
-    } ],
-    read: function() {
-        for (var a in this.sensors) {
-            var b = sensorLib.read(this.sensors[a].type, this.sensors[a].pin);
-            console.log(this.sensors[a].name + ": " +
-              b.temperature.toFixed(1) + "°C, " +
-              b.humidity.toFixed(1) + "%");
-        }
-        setTimeout(function() {
-            sensor.read();
-        }, 2000);
+sensor.read(22, 4, function(err, temperature, humidity) {
+    if (!err) {
+        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+            'humidity: ' + humidity.toFixed(1) + '%'
+        );
     }
-};
-
-sensor.read();
+});
