acbb897ed095224e60ba6b6b861ff7dd12050333
Brad_Hester <hesterbj@iinet.net.au>
2018-01-26 10:42:16 +1100

    Initial Commit

--- Index.js
+++ Index.js
@@ -7,21 +7,17 @@ return new Promise((resolve, reject) => {
     console.log('Starting Temperature...');
         sensor.read(11, 4, function(err, temperature, humidity) {
          if (!err) {
-                console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-                   'humidity: ' + humidity.toFixed(1) + '%'
-                );
+                console.log('temp: ' + temperature.toFixed(1) + '°C');
                 var response = temperature.toFixed(1);
                 console.log(response);
                 resolve(response);
                 }
-
         });
 }).then(function(data){
     return data
 });
 
-//Promise.all([temperaturereturn()]).then(function (data){
-//            console.log('The Temperature is ' + data[0] + '°C');
-//        });
-
+Promise.all([temperaturereturn()]).then(function (data){
+            console.log('The Temperature is ' + data[0] + '°C');
+        });
 
