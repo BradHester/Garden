4b4f4984745ad26e8e78e5ae904531f55e8c0277
Brad_Hester <hesterbj@iinet.net.au>
2018-01-26 10:29:37 +1100

    Initial Commit

--- Index.js
+++ Index.js
@@ -1,9 +1,28 @@
 var sensor = require('node-dht-sensor');
+var gardenconfig = require('config.json');
 
-sensor.read(11, 4, function(err, temperature, humidity) {
-    if (!err) {
-        console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
-            'humidity: ' + humidity.toFixed(1) + '%'
-        );
-    }
+
+var temperaturereturn = function() {
+return new Promise((resolve, reject) => {
+    console.log('Starting Temperature...');
+        sensor.read(11, 4, function(err, temperature, humidity) {
+         if (!err) {
+                console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
+                   'humidity: ' + humidity.toFixed(1) + '%'
+                );
+                let response = temperature.toFixed(1);
+                console.log(response);
+                resolve(response);
+                }
+
+        });
+        });
+}).then(function(data){
+    return data
 });
+
+Promise.all([temperaturereturn()]).then(function (data){
+            console.log('The Temperature is ' + data[0] + '°C');
+        });
+
+
