1fd38629de7910fd68825081dbeb918161364b6c
Brad_Hester <hesterbj@iinet.net.au>
2018-01-26 11:34:45 +1100

    Initial Commit

--- Index.js
+++ Index.js
@@ -8,8 +8,7 @@ return new Promise((resolve, reject) => {
         sensor.read(11, 4, function(err, temperature, humidity) {
          if (!err) {
                 var response = temperature.toFixed(1);
-                //console.log('temp: ' + response + '°C');
-                //console.log(response);
+                console.log('Returning Temperature: ' + response);
                 resolve(response);
                 }
         });
@@ -24,8 +23,7 @@ return new Promise((resolve, reject) => {
         sensor.read(11, 4, function(err, temperature, humidity) {
          if (!err) {
                 var response = humidity.toFixed(1);
-               // console.log('humidity: ' + response + '%');
-               // console.log(response);
+                console.log('Returning Humidity: ' + response);
                 resolve(response);
                 }
         });
@@ -36,15 +34,15 @@ return new Promise((resolve, reject) => {
 
 console.log("Starting gathering...");
 Promise.all([temperaturereturn(),humidityreturn()]).then(function (data){
-      console.log('The Temperature is ' + data[0] + '°C');
+      //console.log('The Temperature is ' + data[0] + '°C');
         var contents = fs.readFileSync('config.json');
         var gardenconfig = JSON.parse(contents);
 
-        console.log('Temperature field Name: ' + gardenconfig.thingspeak.TemperatureFieldName + ' and temperature is ' + data[0]);
-        console.log('Humidity field Name: ' + gardenconfig.thingspeak.HumidityFieldName + ' and humidity is ' + data[1]);
+        //console.log('Temperature field Name: ' + gardenconfig.thingspeak.TemperatureFieldName + ' and temperature is ' + data[0]);
+        //console.log('Humidity field Name: ' + gardenconfig.thingspeak.HumidityFieldName + ' and humidity is ' + data[1]);
 
         var updatestring = gardenconfig.thingspeak.APIURL + '&' +  gardenconfig.thingspeak.TemperatureFieldName + '=' + data[0] + '&' +  gardenconfig.thingspeak.HumidityFieldName + '=' + data[1];
-        console.log(updatestring);
+        console.log('Updating Thingspeak with: ' + updatestring);
 
        // https.get(updatestring + , (response) => {
         //response.on('data', (d) => {
